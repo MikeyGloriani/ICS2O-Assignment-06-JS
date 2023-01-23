8511b8d37f5f4c85287de1d60e56754f1db0e3ba
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2023-01-23 11:04:45 -0500

    added code

--- js/script.js
+++ js/script.js
@@ -8,14 +8,33 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Template-PWA/sw.js", {
-    scope: "/ICS2O-Template-PWA/",
+  navigator.serviceWorker.register("/ICS2O-Assignment-06-JS/sw.js", {
+    scope: "/ICS2O-Assignment-06-JS/",
   })
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-}
+const getJoke = async (URLAddress) => {
+  try {
+    // API fetch
+    const result = await fetch(URLAddress);
+    const jsonData = await result.json();
+    console.log(jsonData);
+
+    //Expression for the jokes
+    if (jsonData) {
+
+      // variable for said jokes
+      const joke = jsonData.joke
+
+      // display joke
+      document.getElementById("hello-world").innerHTML = joke
+    }
+    else {
+      console.log(Error)
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getJoke("https://v2.jokeapi.dev/joke/Any?safe-mode&type=single");
